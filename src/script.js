ac152e00b8545df14d81955404379dbb146f7bdb
Mauricio Dias <mauriciocd7@gmail.com>
2021-03-27 15:41:46 -0300

    Updated img, price, name, desc of the list

--- src/script.js
+++ src/script.js
@@ -5,8 +5,12 @@ pizzaJson.map((item, index)=>{ //mapear a lista utilizando uma arrow function re
     let pizzaItem = c('.models .pizza-item').cloneNode(true); //clone dos itens *cloneNode pega tudo que tem dentro dos itens (classes do html).
 
     //preencher as informações em pizzaItem
-     // recebe name que está no json
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img; //atribuo o item do json ao src pizza item.  
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`; // toFixed- mostra as casas decimais | important, não são aspas simples, são ` para utilizar strings e atributos.
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;     // recebe name que está no json e atribui a classe pizza-item--name.
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
     
+
     c('.pizza-area').append(pizzaItem); //*querySelector - clona | | append adiciona mais um conteúdo, assim como em estrutura de dados.
     
 
