d840bee7574d48899b381c0cd2329882872ce06b
Mauricio Dias <mauriciocd7@gmail.com>
2021-03-27 15:04:45 -0300

    Sintax correction on arrow function

--- src/script.js
+++ src/script.js
@@ -1,10 +1,14 @@
-const c = {element}=>document.querySelector(element); //recebe o elemento e retorna o mesmo para a função. Objetivo e tornar código limpo.
-const cs = {element}=>document.querySelectorAll(element); //retorna um array com os itens que ele achou.
+const c = (element)=>document.querySelector(element); //recebe o elemento e retorna o mesmo para a função. Objetivo e tornar código limpo.
+const cs = (element)=>document.querySelectorAll(element); //retorna um array com os itens que ele achou.
 
 pizzaJson.map((item, index)=>{ //mapear a lista utilizando uma arrow function recebendo 2 parâmetros.
     let pizzaItem = c('.models .pizza-item').cloneNode(true); //clone dos itens *cloneNode pega tudo que tem dentro dos itens (classes do html).
 
     //preencher as informações em pizzaItem
+     // recebe name que está no json
+    
     c('.pizza-area').append(pizzaItem); //*querySelector - clona | | append adiciona mais um conteúdo, assim como em estrutura de dados.
     
+
+
 });
